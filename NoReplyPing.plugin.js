b713b6a319c55639c3c8b061960d782317a33aa8
Qb <qb@qbit.moe>
2023-04-19 03:00:30 +0200

    Initial Commit

---
+++ NoReplyPing.plugin.js
@@ -0,0 +1,45 @@
+/**
+ * @name NoReplyPing
+ * @description Automatically sets replies to not ping the target.
+ * @author Qb
+ * @authorId 133659541198864384
+ * @authorLink https://github.com/BleedingBD/
+ * @version 1.0.0
+ * @invite gj7JFa6mF8
+ * @source https://github.com/BleedingBD/plugin-NoReplyPing/blob/main/NoReplyPing.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/BleedingBD/plugin-NoReplyPing/main/NoReplyPing.plugin.js
+ */
+module.exports = class NoReplyPing {
+    constructor(meta) {
+        this.meta = meta;
+        this.api = new BdApi(meta.name);
+
+        const { Filters } = this.api.Webpack;
+        this.replyBar = this.getModuleAndKey(Filters.byStrings('.shouldMention', '.showMentionToggle'));
+    }
+
+    getModuleAndKey(filter) {
+        const { getModule } = this.api.Webpack;
+        let module;
+        const value = getModule((e, m) => (filter(e) ? (module = m) : false), { searchExports: true });
+        if (!module) return;
+        return [module.exports, Object.keys(module.exports).find((k) => module.exports[k] === value)];
+    }
+
+    start() {
+        if (!this.replyBar) {
+            console.error(`${meta.name}: Unable to start because the reply bar module could not be found.`);
+            return;
+        }
+
+        const { Patcher } = this.api;
+        Patcher.before(...this.replyBar, (_thisArg, [props]) => {
+            props.shouldMention = false;
+        });
+    }
+
+    stop() {
+        const { Patcher } = this.api;
+        Patcher.unpatchAll();
+    }
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# NoReplyPing
+
+This plugin will automatically set replies to not ping the user you are replying to. Using this plugins you will no longer accidentally ping someone when you are trying to reply to them without pinging them.
+
+![Screenshot](https://raw.githubusercontent.com/BleedingBD/plugin-NoReplyPing/main/_meta/thumbnail.png)
+
+## Acknowledgements
+
+This plugin was originally created under the name NoReplyMention by [somebody1234](https://github.com/somebody1234) and later updated by [Strencher](https://github.com/Strencher/) when the original author stopped maintaing it. None of their original code is left in this fork of their plugin.
